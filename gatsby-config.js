18d196bc5f36c53c6a65f40225aa4d5ba6d72254
Diego Andres Cabrera Rojas <diegoandresrojas2000@gmail.com>
2021-01-15 23:08:26 -0500

    react helmet and intro to graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [{ resolve: `gatsby-plugin-emotion` }],
+  siteMetadata: {
+    title: 'Frontend Masters Gatsby Workshop',
+    description: 'A site build with Gatsby',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
 };
